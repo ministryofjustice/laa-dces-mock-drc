8b9f41aca275e8b9f55bc2ef80c529a3c59ae8e0
David Conneely <899881+dconneely@users.noreply.github.com>
2025-02-01 18:54:14 +0000

    Record actual request bodies and unmasked ids

--- dces-mock-drc/Program.cs
+++ dces-mock-drc/Program.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Azure;
 using Azure.Identity;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authentication.Certificate;
@@ -68,6 +69,12 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
 
 var app = builder.Build();
 
+app.Use(next => context =>
+{
+    context.Request.EnableBuffering(); // so we can model-bind AND access the HttpRequest raw body
+    return next(context);
+});
+
 //app.UseAuthentication();
 //app.UseAuthorization();
 
@@ -83,83 +90,130 @@ app.MapGet("/who-am-i", (ClaimsPrincipal user) =>
 });//.RequireAuthorization();
 
 // Keep track of data received.
-int uniqueIndex = 50_000_000;
-var concorContributions = new ConcurrentDictionary<int, ConcorContributionReqRoot>();
-var fdcContributions = new ConcurrentDictionary<int, FdcReqRoot>();
-var concorReqCountByStatus = new ConcurrentDictionary<String, int>();
-var fdcReqCountByStatus = new ConcurrentDictionary<String, int>();
+int devDrcId = 11, devConcorCount = 0, devFdcCount = 0; // increment on valid stores
+var devIdToStatus = new ConcurrentDictionary<int, int>();
+var devPostedRequests = new List<PostedRequest>();
 
-app.MapGet("/laa/v1/stats", () =>
+devIdToStatus.TryAdd(13, 400);
+
+app.MapGet("/data", () =>
 {
-    app.Logger.LogInformation("KeyVault:VaultUri = {}, Alpha:Beta = {}", app.Configuration["KeyVault:VaultUri"], app.Configuration["Alpha:Beta"]);
-    app.Logger.LogInformation("stats request received; responding concorStoredCount:{}, fdcStoredCount:{}",
-        concorContributions.Count, fdcContributions.Count);
+    if (devPostedRequests.Count > 350)
+    {
+        devPostedRequests.RemoveRange(0, devPostedRequests.Count-350);
+    }
     return Results.Ok(new
     {
-        concorStoredCount = concorContributions.Count,
-        fdcStoredCount = fdcContributions.Count,
-        concorRequestCounts = concorReqCountByStatus,
-        fdcRequestCounts = fdcReqCountByStatus
+        concorCount = devConcorCount,
+        fdcCount = devFdcCount,
+        postedRequests = devPostedRequests,
     });
 });//.RequireAuthorization();
 
-app.MapPost("/laa/v1/contribution", ([FromBody] ConcorContributionReqRoot body) =>
+async Task<IResult> HandlePostedRequest(HttpRequest request, string entity, int id, int currentState)
 {
-    if (concorContributions.TryAdd(body.Data.ConcorContributionId, body))
+    request.Body.Position = 0;
+    var rawRequestBody = await new StreamReader(request.Body).ReadToEndAsync();
+    app.Logger.LogInformation("Contribution request received: {}", rawRequestBody);
+
+    switch (currentState)
     {
-        app.Logger.LogInformation("ConcorContribution payload received {}; responding 200 OK", body);
-        concorReqCountByStatus.AddOrUpdate("200 OK", 1, (status, count) => count + 1);
-        return Results.Ok(new
-        {
-            drcId = Interlocked.Increment(ref uniqueIndex).ToString(),
-            concorContributionId = body.Data.ConcorContributionId
-        });
+        case 200:
+            devPostedRequests.Add(new PostedRequest(DateTime.Now, request.Path, id,  rawRequestBody, currentState, "OK with drcId", entity));
+            bool inc = devIdToStatus.TryUpdate(id, 409, currentState);
+            if ("Fdc".Equals(entity))
+            {
+                if (inc) Interlocked.Increment(ref devFdcCount);
+                return Results.Ok(new { meta = new {
+                        drcId = Interlocked.Increment(ref devDrcId),
+                        fdcId = id } });
+            }
+            else
+            {
+                if (inc) Interlocked.Increment(ref devConcorCount);
+                return Results.Ok(new { meta = new {
+                        drcId = Interlocked.Increment(ref devDrcId),
+                        concorContributionId = id } });
+            }
+        case 635:
+            devPostedRequests.Add(new PostedRequest(DateTime.Now, request.Path, id, rawRequestBody, 200, "OK without drcId", entity));
+            devIdToStatus.TryUpdate(id, 409, currentState);
+            return Results.Ok();
+        case 409:
+            devPostedRequests.Add(new PostedRequest(DateTime.Now, request.Path, id, rawRequestBody, currentState, "Conflict with duplicate-id", ""));
+            return Results.Problem(
+                $"The {entity} [{id}] already exists in the database",
+                request.Path,
+                currentState,
+                "Conflict",
+                "https://laa-debt-collection.service.justice.gov.uk/problem-types#duplicate-id");
+        case 659:
+            devPostedRequests.Add(new PostedRequest(DateTime.Now, request.Path, id, rawRequestBody, 409, "Conflict without duplicate-id", entity));
+            return Results.Conflict();
+        case 400:
+            devPostedRequests.Add(new PostedRequest(DateTime.Now, request.Path, id, rawRequestBody, currentState, "Bad Request with problemDetail", ""));
+            return Results.Problem(
+                $"Validation failed in some unspecified way",
+                request.Path,
+                currentState);
+        default:
+            devPostedRequests.Add(new PostedRequest(DateTime.Now, request.Path, id, rawRequestBody, 500, "Internal Server Error with problemDetails", ""));
+            return Results.Problem(
+                $"statusCode {currentState}",
+                request.Path,
+                500);
+    }
+}
+
+app.MapPost("/laa/v1/contribution", async (HttpRequest request, [FromBody] ConcorContributionReqRoot body) =>
+{
+    // Do we have a "preplanned" response? If not, respond 200 this time, and add 409 as next response:
+    if (devIdToStatus.TryAdd(body.Data.ConcorContributionId, 200))
+    {
+        app.Logger.LogInformation("Contribution payload received {}; no predetermined state", body);
+        return await HandlePostedRequest(request, "Contribution", body.Data.ConcorContributionId, 200);
     }
     else
     {
-        app.Logger.LogInformation("ConcorContribution payload received {}; responding 409 Conflict", body);
-        concorReqCountByStatus.AddOrUpdate("409 Conflict", 1, (status, count) => count + 1);
-        return Results.Problem( // use our own spelling mistake (concur instead of condor):
-            $"The concurContributionId [{body.Data.ConcorContributionId}] already exists in the database",
-            "/api/contribution",
-            409,
-            "Conflict",
-            "https://laa-debt-collection.service.justice.gov.uk/problem-types#duplicate-id");
+        int currentState = devIdToStatus[body.Data.ConcorContributionId];
+        app.Logger.LogInformation("ConcorContribution payload received {}; state {}", body, currentState);
+        return await HandlePostedRequest(request, "Contribution", body.Data.ConcorContributionId, currentState);
     }
 });//.RequireAuthorization();
 
-app.MapPost("/laa/v1/fdc", ([FromBody] FdcReqRoot body) =>
+app.MapPost("/laa/v1/fdc", async (HttpRequest request, [FromBody] FdcReqRoot body) =>
 {
-    if (fdcContributions.TryAdd(body.Data.FdcId, body))
+    // Do we have a "preplanned" response? If not, respond 200 this time, and add 409 as next response:
+    if (devIdToStatus.TryAdd(body.Data.FdcId, 200))
     {
-        app.Logger.LogInformation("FDC payload received {}; responding 200 OK", body);
-        fdcReqCountByStatus.AddOrUpdate("200 OK", 1, (status, count) => count + 1);
-        return Results.Ok(new
-        {
-                drcId = Interlocked.Increment(ref uniqueIndex).ToString(),
-                fdcId = body.Data.FdcId
-        });
+        app.Logger.LogInformation("Fdc payload received {}; no predetermined state", body);
+        return await HandlePostedRequest(request, "Fdc", body.Data.FdcId, 200);
     }
     else
     {
-        app.Logger.LogInformation("FDC payload received {}; responding 409 Conflict", body);
-        fdcReqCountByStatus.AddOrUpdate("409 Conflict", 1, (status, count) => count + 1);
-        return Results.Problem(
-            $"The fdcId [{body.Data.FdcId}] already exists in the database",
-            "/api/fdc",
-            409,
-            "Conflict",
-            "https://laa-debt-collection.service.justice.gov.uk/problem-types#duplicate-id");
+        int currentState = devIdToStatus[body.Data.FdcId];
+        app.Logger.LogInformation("Fdc payload received {}; state {}", body, currentState);
+        return await HandlePostedRequest(request, "Fdc", body.Data.FdcId, currentState);
     }
 });//.RequireAuthorization();
 
 app.Run();
 
+// We keep a list of the last N requests
+[UsedImplicitly] record PostedRequest(
+    DateTime DateTime,
+    string UriPath,
+    int Id,
+    string RequestBody,
+    int StatusCode,
+    string ResponseType,
+    string StoredType);
+
 // DTOs for JSON request bodies
 [UsedImplicitly] record ConcorContributionReqObj(int MaatId, String Flag);
 [UsedImplicitly] record ConcorContributionReqData(int ConcorContributionId, ConcorContributionReqObj ConcorContributionObj);
-record ConcorContributionReqRoot([UsedImplicitly] ConcorContributionReqData Data, JsonElement Meta);
+[UsedImplicitly] record ConcorContributionReqRoot(ConcorContributionReqData Data, JsonElement Meta);
 
 [UsedImplicitly] record FdcReqObj(long MaatId);
 [UsedImplicitly] record FdcReqData(int FdcId, FdcReqObj FdcObj);
-record FdcReqRoot([UsedImplicitly] FdcReqData Data, JsonElement Meta);
+[UsedImplicitly] record FdcReqRoot(FdcReqData Data, JsonElement Meta);
